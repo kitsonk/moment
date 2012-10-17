fd71f4bc574cf622ca5d6e182681a34e4467fb2d
Kitson Kelly <dojo@kitsonkelly.com>
2012-10-17 19:48:21 +0100

    repackage for dojo

--- moment.js
+++ moment.js
@@ -4,7 +4,7 @@
 // license : MIT
 // momentjs.com
 
-(function (undefined) {
+define([], function(){
 
     /************************************
         Constants
@@ -1192,22 +1192,5 @@
         Exposing Moment
     ************************************/
 
-
-    // CommonJS module is defined
-    if (hasModule) {
-        module.exports = moment;
-    }
-    /*global ender:false */
-    if (typeof ender === 'undefined') {
-        // here, `this` means `window` in the browser, or `global` on the server
-        // add `moment` as a global object via a string identifier,
-        // for Closure Compiler "advanced" mode
-        this['moment'] = moment;
-    }
-    /*global define:false */
-    if (typeof define === "function" && define.amd) {
-        define("moment", [], function () {
-            return moment;
-        });
-    }
-}).call(this);
+    return moment;
+});
